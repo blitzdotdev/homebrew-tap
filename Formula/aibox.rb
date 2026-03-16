207eff7370c7dff3ecdd801d7eae9cc94ecf80ee
Palash Bansal <palashbansal96@gmail.com>
2026-03-17 02:56:17 +0530

    Add aibox formula

---
+++ Formula/aibox.rb
@@ -0,0 +1,18 @@
+class Aibox < Formula
+  desc "Run AI coding agents in isolated Docker containers"
+  homepage "https://github.com/blitzdotdev/aibox"
+  url "https://github.com/blitzdotdev/aibox/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 ""
+  license "MIT"
+
+  depends_on "docker" => :recommended
+  depends_on "docker-compose" => :recommended
+
+  def install
+    bin.install "bin/aibox"
+  end
+
+  test do
+    assert_match "aibox v", shell_output("#{bin}/aibox version")
+  end
+end
